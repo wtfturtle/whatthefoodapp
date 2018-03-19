b563fb2bd0e302fae5e2af8436ac2956db2ee4d5
Ivan Limongan <ivan.limongan@gmail.com>
2018-03-18 17:48:14 -0700

    burger added

--- src/components/app/header/Header.js
+++ src/components/app/header/Header.js
@@ -13,12 +13,19 @@ class Header extends Component {
       <header role="banner" id="header">
         <section className="head-container maxwidth-wrap">
           <Link to="/" className="no-line"><h1 className="logo">What The Food</h1></Link>
-          <nav>
-            <ul className="nav-ul">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/login">Log In</Link></li>
-            </ul>
-          </nav>
+          
+          <div>
+            <label htmlFor="check" name="checkbox"></label>
+            <input id="check" className="checkbox" type="checkbox"/>
+
+            <div id="burger"><span></span><span></span><span></span></div>
+            <nav id="main-menu">
+              <ul className="nav-ul">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Log In</Link></li>
+              </ul>
+            </nav>
+          </div>
           {error && <Error error={error}/>}
         </section> 
       </header>

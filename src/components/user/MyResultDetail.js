7e2d1412b9bc121ec434e0416eef0d82c60e03c4
Ivan Limongan <ivan.limongan@gmail.com>
2018-03-23 12:28:07 -0700

    final style user detail

--- src/components/user/MyResultDetail.js
+++ src/components/user/MyResultDetail.js
@@ -8,6 +8,7 @@ import Rating from '../edit/Rating';
 import Notes from '../edit/Notes';
 import AddDetail from '../buttons/AddDetail';
 import RemoveDetail from '../buttons/RemoveDetail';
+import '../results/result.css';
 
 
 class MyResultDetail extends Component {
@@ -42,40 +43,70 @@ class MyResultDetail extends Component {
     const { message } = result.venue.price || 'Not Listed';
     const { user, loadSaveResults, venueLoad } = this.props;
 
+ 
+
     return (
-      <div>
-        <div>
-          <div>
-            <Link to="/">⬅ Back</Link>
-            <img src={imageUrl} alt="restaurant"></img>
-          </div>
+      <section className="main-container maxwidth-wrap">
+        <div className="body-padding">
+          <Link className="back" to="/">⬅ Back to results</Link>
+          <div className="detail-flex">
 
-          <div>
-            <h3>{name}</h3> 
-            <p>Price: {message}</p> 
-            <p><a href={url} alt={name}>{url}</a></p>
-            <p>{phone}</p>
-            <p>{address}</p>
-            <p>{city}</p>
-            {user && loadSaveResults &&
-              (venueLoad[id][listKey] ? 
-                <RemoveDetail venue={result.venue}/> 
-                :  
-                <AddDetail venue={result.venue}/>
-              )        
-            } 
-          </div>
-        </div>
-
-        {user && 
-          (loadSaveResults.includes(id) && 
             <div>
-              <Rating/>
-              <Notes id={id}/>
-            </div> 
-          ) 
-        }
-      </div>
+              <img className="detail-img" src={imageUrl} alt={name}></img>
+              <div>
+                <div className="info-flex">
+                  <h3>{name}</h3> 
+                  <p className="top-pad"><Link to={url} alt={name} target="_blank" rel="author noopener noreferrer"><span className="weblink">Visit Online</span></Link></p>
+                </div>
+
+                <div className="info-flex subtop">
+                  <div>
+                    <span className="sub">Price</span>
+                    <p>{message}</p> 
+                  </div>
+                  <div>
+                    <span className="sub">Phone</span>
+                    <p>{phone}</p>
+                  </div>
+                  <div>
+                    <span className="sub">Address</span>
+                    <p>{address}<br/>{city}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           
+            <div className="edit-detail">
+              {user && loadSaveResults &&
+              (venueLoad[id][listKey] ? 
+                <div>
+                  <h4>Save to List</h4>
+                  <RemoveDetail venue={result.venue}/> 
+                </div>
+                :  
+                <div>
+                  <h4>Save to List</h4>
+                  <AddDetail venue={result.venue}/>
+                </div>
+              )        
+              } 
+              <br/><hr className="hr"/>
+        
+
+              {user && 
+                (loadSaveResults.includes(id) && 
+                  <div>
+                    <Rating/>
+                    <hr className="hr"/>
+                    <Notes id={id}/>
+                  </div> 
+                ) 
+              }
+            </div>
+          </div>
+          <Link className="back" to="/">⬅ Back to results</Link>
+        </div>
+      </section>
     );
   }
 }
